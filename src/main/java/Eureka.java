4986f78702940c29f38ca550994e233ad7d8dae1
subiloble <e0773758@u.nus.edu>
2025-01-24 23:35:42 +0800

    Level 4

--- src/main/java/Eureka.java
+++ src/main/java/Eureka.java
@@ -1,5 +1,81 @@
 import java.util.Scanner;
 
+enum TaskType {
+    todo, deadline, event
+}
+
+enum Command {
+    bye, list, mark, unmark, todo, deadline, event, unknown
+}
+
+abstract class Task {
+    String description;
+    boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    public String getStatus() {
+        return (isDone ? "[X]" : "[ ]");
+    }
+
+    @Override
+    public String toString() {
+        return getStatus() + " " + description;
+    }
+}
+
+class ToDo extends  Task {
+    public ToDo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+class Deadline extends Task {
+    String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
+
+class Event extends Task {
+    String from;
+    String to;
+
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+    }
+}
+
 public class Eureka {
     static void printLine() {
         System.out.print("  ");
@@ -11,10 +87,29 @@ public class Eureka {
         System.out.println();
     }
 
+    static Command parseCommand(String userInput) {
+        if (userInput.equals("bye")) {
+            return Command.bye;
+        } else if (userInput.equals("list")) {
+            return Command.list;
+        } else if (userInput.startsWith("mark ")) {
+            return Command.mark;
+        } else if (userInput.startsWith("unmark ")) {
+            return Command.unmark;
+        } else if (userInput.startsWith("todo ")) {
+            return Command.todo;
+        } else if (userInput.startsWith("deadline ")) {
+            return Command.deadline;
+        } else if (userInput.startsWith("event ")) {
+            return Command.event;
+        } else {
+            return Command.unknown;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
-        boolean[] isDone = new boolean[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         String logo = "  ______ _    _ _____  ______ _   __      __       \n"
@@ -30,45 +125,91 @@ public class Eureka {
 
         while (true) {
             String input = scanner.nextLine();
-            if (input.equals("bye")) {
-                printLine();
-                System.out.println("  Bye. Hope to see you again soon!");
-                printLine();
-                break;
-            } else if (input.equals("list"))  {
-                printLine();
-                for (int i = 0; i < taskCount; i++) {
-                    String status = isDone[i] ? "[X]" : "[ ]";
-                    System.out.println("  " + (i + 1) + ". " + status + " " + tasks[i]);
-                }
-                printLine();
-            } else if (input.startsWith("mark ")) {
-                int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
-                if (taskNumber >= 0 && taskNumber < taskCount) {
-                    isDone[taskNumber] = true;
+            Command command = parseCommand(input);
+
+            switch (command) {
+                case bye:
                     printLine();
-                    System.out.println("  Nice! I've marked this task as done:");
-                    System.out.println("    [X] " + tasks[taskNumber]);
+                    System.out.println("  Bye. Hope to see you again soon!");
                     printLine();
-                }
-            } else if (input.startsWith("unmark ")) {
-                int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
-                if (taskNumber >= 0 && taskNumber < taskCount) {
-                    isDone[taskNumber] = false;
+                    scanner.close();
+                    return;
+
+                case list:
                     printLine();
-                    System.out.println("  OK, I've marked this task as not done yet:");
-                    System.out.println("    [ ] " + tasks[taskNumber]);
+                    System.out.println("  Here are the tasks in your list:");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println("  " + (i + 1) + ". " + tasks[i]);
+                    }
                     printLine();
-                }
-            } else {
-                printLine();
-                tasks[taskCount] = input;
-                isDone[taskCount] = false;
-                taskCount++;
-                System.out.println("  added: " + input);
-                printLine();
+                    break;
+
+                case mark:
+                    int markTaskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
+                    if (markTaskNumber >= 0 && markTaskNumber < taskCount) {
+                        tasks[markTaskNumber].markAsDone();
+                        printLine();
+                        System.out.println("  Nice! I've marked this task as done:");
+                        System.out.println("    " + tasks[markTaskNumber]);
+                        printLine();
+                    }
+                    break;
+
+                case unmark:
+                    int unmarkTaskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
+                    if (unmarkTaskNumber >= 0 && unmarkTaskNumber < taskCount) {
+                        tasks[unmarkTaskNumber].markAsNotDone();
+                        printLine();
+                        System.out.println("  OK, I've marked this task as not done yet:");
+                        System.out.println("    " + tasks[unmarkTaskNumber]);
+                        printLine();
+                    }
+                    break;
+
+                case todo:
+                    String todoDescription = input.substring(5);
+                    tasks[taskCount] = new ToDo(todoDescription);
+                    taskCount++;
+                    printLine();
+                    System.out.println("  Got it. I've added this task:");
+                    System.out.println("    " + tasks[taskCount - 1]);
+                    System.out.println("  Now you have " + taskCount + " tasks in the list.");
+                    printLine();
+                    break;
+
+                case deadline:
+                    String[] deadlineParts = input.substring(9).split(" /by ");
+                    String deadlineDescription = deadlineParts[0];
+                    String deadlineBy = deadlineParts[1];
+                    tasks[taskCount] = new Deadline(deadlineDescription, deadlineBy);
+                    taskCount++;
+                    printLine();
+                    System.out.println("  Got it. I've added this task:");
+                    System.out.println("    " + tasks[taskCount - 1]);
+                    System.out.println("  Now you have " + taskCount + " tasks in the list.");
+                    printLine();
+                    break;
+
+                case event:
+                    String[] eventParts = input.substring(6).split(" /from | /to ");
+                    String eventDescription = eventParts[0];
+                    String eventFrom = eventParts[1];
+                    String eventTo = eventParts[2];
+                    tasks[taskCount] = new Event(eventDescription, eventFrom, eventTo);
+                    taskCount++;
+                    printLine();
+                    System.out.println("  Got it. I've added this task:");
+                    System.out.println("    " + tasks[taskCount - 1]);
+                    System.out.println("  Now you have " + taskCount + " tasks in the list.");
+                    printLine();
+                    break;
+
+                default:
+                    printLine();
+                    System.out.println("  I'm sorry, I don't understand that command.");
+                    printLine();
+                    break;
             }
         }
-        scanner.close();
     }
 }
